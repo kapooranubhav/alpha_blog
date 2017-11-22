00ba26b059dce7b8f7767dfae78fe6ebef4748f3
Anubhav Kapoor <kapoor.anubhav@gmail.com>
2017-11-22 17:01:09 +0000

    Extract redundancy and production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,25 @@
 class ArticlesController < ApplicationController
 
-def index
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-  @articles = Article.all
+  def index
 
-end 
+    @articles = Article.all
 
-def new 
+  end 
+
+  def new 
   
-  @article = Article.new
-end 
+    @article = Article.new
+  end 
 
-def edit
+  def edit
 
-  @article = Article.find(params[:id])
+  end 
 
-end 
+  def create 
 
-def create 
-
-  @article = Article.new(article_params)
+    @article = Article.new(article_params)
   
   if @article.save 
     flash[:notice] = "Article was successfuly created"
@@ -27,16 +27,16 @@ def create
   
   else
     
-    render 'new'
+      render 'new'
   
   end 
 
-end 
+  end 
 
 
-def update 
+  def update 
 
-  @article = Article.find(params[:id])
+  
   if @article.update(article_params)
     flash[:notice] = "Article was successfuly updated"
     redirect_to article_path(@article)
@@ -45,25 +45,31 @@ def update
     render 'edit'
   end
 
-end 
+  end 
 
 
-def show 
-  @article = Article.find(params[:id])
-end
+  def show 
+  
+  end
 
 
-def destroy
-    @article = Article.find(params[:id])
+  def destroy
+   
     @article.destroy
     flash[:notice] = "Article was successfuly deleted"
     redirect_to articles_path
-end 
+  end 
 
-private 
+  private 
 
+    def set_article
+      @article = Article.find(params[:id])
+    end 
+    
+    
     def article_params
       params.require(:article).permit(:title, :description)
     end 
 
+    
 end 
