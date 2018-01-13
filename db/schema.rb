73418ad6be42344f95cbbd1cff50264f5cbd6ab8
Anubhav Kapoor <kapoor.anubhav@gmail.com>
2018-01-13 19:31:14 +0000

    Checking the User Table

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180111163023) do
   end
 
   create_table "users", force: :cascade do |t|
+
     t.string   "username"
     t.string   "email"
     t.datetime "created_at", null: false
